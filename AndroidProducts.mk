25a486cd7e79f95cab3fc62628bb7b46fae6fd8c
Momo <momo5418@outlook.com>
2023-12-23 08:48:05 +0800

    1

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_MX6.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_MX6-user \
+    omni_MX6-userdebug \
+    omni_MX6-eng
